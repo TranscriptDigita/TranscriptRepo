5dab356cedbeb5c3852951c514f3447763ca24c8
Semmwa <bakshaksemmwa@gmail.com>
2023-07-06 13:28:21 +0100

    fixing signup design

--- client/src/components/Input.jsx
+++ client/src/components/Input.jsx
@@ -3,7 +3,7 @@ const Input = (props) => {
 
   const { type, name, placeholder, id, } = props
   return (
-      <input type={type} name={name} placeholder={placeholder} id={id} className="bg-indigo-100 border border-indigi-300  block w-full text-gray-700 rounded-lg py-1.5 px-4 mb-3 leading-tight"/>
+      <input type={type} name={name} placeholder={placeholder} id={id} className="bg-gray-100 border border-gray-500  block w-full text-gray-700 rounded-lg py-1.5 px-4 mb-3 leading-tight"/>
   )
 }
 
--- client/src/pages/auth/views/Signup.jsx
+++ client/src/pages/auth/views/Signup.jsx
@@ -1,23 +1,32 @@
+imp
 import Input from "../../../components/Input"
 import InputLabel from "../../../components/InputLabel"
 import Button from "../../../components/Button"
 import {FaLinkedin} from "react-icons/fa"
 import {ImGooglePlus3} from "react-icons/im"
 const Signup = () => {
+  
+  const [formData, ser] = {
+
+  }
+
+
   return (
     <div className="bg-indigo-800">
-        <div className="bg-white m-auto w-7/12 p-5">
-        <h2 className="text-2xl mb-2 text-center">Sign Up With</h2>
-        <div className="flex flex-wrap w-full m-auto justify-center">
-            <div className ="w-3/6 px-16 md:mb-0">
-                {/* <InputLabel label="FirstName" /> */}
-                <FaLinkedin/>
-                {/* <Input type="text" /> */}
+        <div className="bg-indigo-100 m-auto w-7/12 px-4 pt-2">
+        <h2 className="text-3xl mb-2 text-center font-semibold">Sign Up With</h2>
+        <div className="flex flex-row w-full justify-center">
+            <div className ="w-5/6 px-8 md:mb-0">
+                <a href="" className="w-4/6 border bg-gray-50 border-gray-500 p-1 rounded-lg flex flex-row items-baseline justify-around m-auto border-b-4 border-r-2 border-l-2">
+                    <FaLinkedin/>
+                    <p className="">Sign In with LinkedIn</p>
+                </a>
             </div>
-            <div className ="w-3/6 px-16 md:mb-0">
-                {/* <InputLabel label="LastName" /> */}
-                <ImGooglePlus3/>
-                {/* <Input type="text" /> */}
+            <div className ="w-5/6 px-8 md:mb-0">
+                <a className="w-4/6 border border-gray-500 p-1 rounded-lg flex flex-row items-baseline justify-around m-auto bg-gray-50 border-b-4 border-r-2 border-l-2">
+                    <ImGooglePlus3/>
+                    <p>Sign In with Google</p>
+                </a>
             </div>
         </div>
         <h2 className="text-2xl mb-2 text-center">OR</h2>
