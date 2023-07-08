5bfe03bedf50c77127925d912efd1df6f999f5e7
Semmwa <bakshaksemmwa@gmail.com>
2023-07-08 13:44:32 +0100

    fixed home

--- client/src/pages/main/Home.jsx
+++ client/src/pages/main/Home.jsx
@@ -2,7 +2,10 @@
 const Home = () => {
   return (
     <div>
-      
+      TRANSCRIPT DIGITA
+
+      <div>Login here<a href="/login"></a></div>
+      <div>Signup here<a href="/signup"></a></div>
     </div>
   )
 }
