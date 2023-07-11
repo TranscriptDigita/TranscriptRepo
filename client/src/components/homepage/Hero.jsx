fe47b3e18f7aa89e4fac28f923a4e468f004e02b
Semmwa <bakshaksemmwa@gmail.com>
2023-07-11 11:24:18 +0100

    fixing empty spaces

--- client/src/components/homepage/Hero.jsx
+++ client/src/components/homepage/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
       <div >
         <NavBar/>
       </div>
-      <div className="w-full md:w-4/6 m-auto text-center pt-8">
+      <div className="w-full md:w-4/6 m-auto text-center pt-8 pb-2">
         <h2 className="text-2xl font-semibold sm:w-full md:text-4xl">Get your transcript delivered </h2>
         <h2 className="text-2xl font-semibold sm:w-full md:text-4xl">with ease worldwide</h2>
       </div>
@@ -17,7 +17,7 @@ const Hero = () => {
       <div className="w-5/6 font-semibold text-xl m-auto text-center">
         <h3>Get your transcript delivered quickly and accurately with Transcript Digita from your school to anywhere in the world without stress. 100% Efficient and Reliable</h3>
       </div> 
-      <div className="flex justify-around pt-3 w-4/12 m-auto">
+      <div className="flex justify-around pt-4 w-4/12 m-auto">
         <button className=" font-semibold px-6 py-2 text-white bg-primary hover:bg-white hover:text-black hover:border border-black rounded-lg">Track Request</button>
         <button className="font-semibold px-6 py-2 text-white bg-primary hover:bg-white hover:text-black hover:border border-black rounded-lg">Get Started</button>
       </div>
