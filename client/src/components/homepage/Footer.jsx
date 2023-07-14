34e5eff95fde11d28df5f154e3bbebf77384fe37
Semmwa <bakshaksemmwa@gmail.com>
2023-07-14 19:44:42 +0100

    ready view of landing page

--- client/src/components/homepage/Footer.jsx
+++ client/src/components/homepage/Footer.jsx
@@ -1,3 +1,6 @@
+import insta from "../../assets/Instagram.png"
+import facebook from "../../assets/facebook.png"
+import twitter from "../../assets/twitter.png"
 
 const Footer = () => {
   return (
@@ -9,9 +12,9 @@ const Footer = () => {
           <p className="text-sm">The leading transcript service in Nigeria, providing reliable, fast services accross the nation and worldwide</p>
          </div>
         </div>
-        <div className="col-span-4 text-sm">
+        <div className="col-span-4 text-sm ">
           <h4 className="font-medium text-xl py-3">QuickLinks</h4>
-          <p ><a href="/about">About</a></p>
+          <p><a href="/about" className="hover:border-b-white">About</a></p>
           <p><a href="/testimonial">Testimonial</a></p>
           <p><a href="/faq">FAQ</a></p>
         </div>
@@ -19,10 +22,10 @@ const Footer = () => {
           <h4 className="font-medium text-xl py-3">Contacts</h4>
           <p>+234-943827323</p>
           <p>Transdigita@gmail.com</p>
-          <div className="flex flex-row gap-5">
-            <p>I</p>
-            <p>T</p>
-            <p>F</p>
+          <div className="flex flex-row gap-4 pt-1">
+            <img src={insta} alt="" className="w-4.5 h-5"/>
+            <img src={facebook} alt="" className="w-5 h-5"/>
+            <img src={twitter} alt="" className="w-4.5 h-5"/>
           </div>
         </div>
       </div>
--- client/src/components/homepage/NewsLetter.jsx
+++ client/src/components/homepage/NewsLetter.jsx
@@ -1,4 +1,4 @@
-import Input from "../form/Input"
+// import Input from "../form/Input"
 import Button from "../form/Button"
 
 const NewsLetter = () => {
@@ -7,20 +7,25 @@ const NewsLetter = () => {
    <div className="flex flex-row p-12">
       <div className="flex-1">
         <div className="w-full text-center">
-        <h3 className="text-2xl font-semibold">Our Newsletter</h3>
+        <h3 className="text-2xl font-semibold pt-2">Our Newsletter</h3>
         </div>
       </div>
       <div className="flex-1">
-       <div className="w-full flex flex-row">
-       <div className="px-4">
-          <Input placeholder="enter your email" />
-        </div>
-        <div className="px-1">
-          <Button btn="Subscribe"/>
+       <div className="w-full flex flex-row align-baseline">
+         <div className="relative flex items-center mt-2">
+          <span className="absolute">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 mx-3 text-gray-400 dark:text-gray-500">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
+              </svg>
+          </span>
+          <input type="email" placeholder="john@example.com" className="block w-full py-1 text-gray-700 placeholder-gray-400/70 bg-white border border-gray-500 rounded-lg pl-11 pr-5 rtl:pr-11 rtl:pl-5 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-indigo-400 dark:focus:border-indigo-300 focus:ring-indigo-300 focus:outline-none focus:ring focus:ring-opacity-40" />
+          </div>
+          <div className="px-1 pt-2">
+            <Button btn="Subscribe"/>
+          </div>
         </div>
        </div>
       </div>
-    </div>
     </div>
   )
 }
