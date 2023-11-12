2c9db0c4b7d1460b10c60405dd55aa0da0a85a58
Godwin <67944336+UgbeGodwinIkpe@users.noreply.github.com>
2023-11-12 12:48:45 +0100

    updates

--- backend/v1/routes/institution.js
+++ backend/v1/routes/institution.js
@@ -4,6 +4,8 @@
 const express = require('express'),
     router = express.Router(),
     controller = require('../controllers/institution'),
+    accountController = require('../controllers/bankAccount'),
+    resultController = require('../controllers/studentsData'),
     { isAuth } = require('../middleware/auth')
 
 // =============================
@@ -20,6 +22,10 @@ router.route('/:id/verify')
     //============================
     // =======New endpoints========
 router.route('/account')
-    .post(isAuth, controller.setupBankAccountDetails)
+    .post(isAuth, accountController.setupBankAccount)
+
+router.route('/students-records')
+    .post(isAuth, resultController.uploadData)
+
 
 module.exports = router
