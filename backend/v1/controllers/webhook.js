a6e5f9492630152ec5842981a4c171049d8cca02
Godwin <67944336+UgbeGodwinIkpe@users.noreply.github.com>
2024-02-11 16:20:03 +0100

    changes and updates

--- backend/v1/controllers/webhook.js
+++ backend/v1/controllers/webhook.js
@@ -12,7 +12,7 @@ const webhook = (req, res) => {
         console.log(event);
         // Do something with event  
     }
-    res.send(200);
+    res.sendStatus(200);
 }
 
 // export webhook controller
