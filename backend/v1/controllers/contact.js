5492176283a9547bfcbb01a6a0caf436bc50df22
Godwin <67944336+UgbeGodwinIkpe@users.noreply.github.com>
2024-02-17 15:08:42 +0100

    changes made

--- backend/v1/controllers/contact.js
+++ backend/v1/controllers/contact.js
@@ -1,9 +1,10 @@
 // Function to send email
-const sendEmail = async function(email, subject, message) {
+const sendEmail = async function(email, subject, message, em) {
         let transport = nodemailer.createTransport(smtpTransport({
-            host: 'smtp.gmail.com',
-            secure: true,
-            port: 465,
+            service: 'gmail',
+            host: process.env.EMAIL_HOST,
+            secure: false, // 465,
+            port: 587,
             auth: {
                 user: process.env.EMAIL_USERNAME,
                 pass: process.env.EMAIL_PASSWORD
@@ -12,9 +13,9 @@ const sendEmail = async function(email, subject, message) {
 
         const info = await transport.sendMail({
             from: process.env.EMAIL_USERNAME,
-            to: email,
+            to: em,
             subject: subject,
-            html: message
+            html: message + ' ' + email
 
         }, (err, sent) => {
             err ? res.status(505).json({ message: 'Error ocuured while sending your message.', err }) : res.status(200).json({ message: 'Successully sent', sent })
@@ -23,15 +24,15 @@ const sendEmail = async function(email, subject, message) {
     }
     //contact us function
 exports.contactUs = async(req, res) => {
-
-        // destructuring request body
+        const em = "transcriptdigita@gmail.com"
+            // destructuring request body
         let { name, email, message } = req.body
         let subject = `${name} Contacting`
 
         try {
 
             // sending the contact us message
-            await sendEmail(email, subject, message)
+            await sendEmail(email, subject, message, em)
 
             // return status code and data as json
             return res.status(200).json({ message: 'Successully sent' })
